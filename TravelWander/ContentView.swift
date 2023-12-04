1fe542d6d6e0d121686d3c69b561bbf89f545006
Ed Penano <hi@edpenano.com>
2023-12-04 11:38:14 -0800

    Initial Commit

---
+++ TravelWander/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TravelWander
+//
+//  Created by Ed Penano on 12/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TravelWander/TravelWanderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelWanderApp.swift
+//  TravelWander
+//
+//  Created by Ed Penano on 12/4/23.
+//
+
+import SwiftUI
+
+@main
+struct TravelWanderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
